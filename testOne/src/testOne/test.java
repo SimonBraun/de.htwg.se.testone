1a0d6956eaacb9fd9e83bba4be50b3cabe858b31
DennisMoosmann <dennis.moosmann@gmx.de>
2013-10-11 12:51:05 +0200

    Hallo

--- testOne/src/testOne/test.java
+++ testOne/src/testOne/test.java
@@ -3,7 +3,7 @@ package testOne;
 public class test {
 
 	public static void main (String [] args) {
-		System.out.println("Hallo");
+		System.out.println("Hallo Dennis");
 		//This is me, Timo
 	}
 }
